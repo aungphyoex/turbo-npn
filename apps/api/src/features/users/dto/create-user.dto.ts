d8e02acaae4186ab4eed803b98cb6558dbea9e4d
Halitcan Çıkıkçı <halitcancikikci@gmail.com>
2025-02-08 15:29:40 +0300

    feat: detailed swagger documentations

--- apps/api/src/features/users/dto/create-user.dto.ts
+++ apps/api/src/features/users/dto/create-user.dto.ts
@@ -1,24 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User email address',
+    example: 'user@example.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'User password',
+    example: 'strongPassword123',
+  })
   @IsString({
     message: 'Password must be a string',
   })
   password: string;
 
+  @ApiProperty({
+    description: 'Username',
+    example: 'johndoe',
+  })
   @IsString({
     message: 'Username must be a string',
   })
   username: string;
 
+  @ApiProperty({
+    description: 'Full name of the user',
+    example: 'John Doe',
+  })
   @IsString({
     message: 'Name must be a string',
   })
   name: string;
 
+  @ApiProperty({
+    description: 'Active status of the user',
+    example: true,
+  })
   @IsBoolean({
     message: 'Active status must be a boolean',
   })
--- apps/api/src/features/users/users.controller.ts
+++ apps/api/src/features/users/users.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, description: 'Users fetched successfully' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiBearerAuth()
   @Get()
   async findAll() {
     const data = await this.usersService.findAll();
